4fc7b8d72e3e2e708e10f07ce9e46f89a08a3754
Nabreet Ghumman <nabreetghumman@Nabs-MBP.hitronhub.home>
2019-05-07 20:56:20 -0700

    initial commit

---
+++ n_queen_ backtracking.py
@@ -0,0 +1,19 @@
+class queen():
+    def __init__(self, board):
+        self.board = board
+        self.board_size = len(board)
+
+    #print n*n board
+    def print_board(self, board = None):
+        for row in self.board:
+            for col in self.board:
+                if(row == board[col]):
+                    print("Q", end = "\t")
+                else:
+                    print('_', end="\t")
+            print("\n")
+
+
+board= [0,1,2,3,4]
+board1= queen(board)
+board1.print_board(board)
