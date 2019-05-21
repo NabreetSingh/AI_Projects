0ea5c6dea5aa5006d433e639923377cf061444ff
My Pc <nabreetghumman@hotmail.com>
2019-05-20 23:40:39 -0700

    function to count conflict

--- n_queen_ backtracking.py
+++ n_queen_ backtracking.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 class queen():
     def __init__(self, board_size):
         self.board = board
@@ -5,16 +6,50 @@ class queen():
 
     #backtracking algorithn to place queen one by one safely on the board
     def place_queens(self):
+        pass
+
+    def count_conflicts(self, column):
+        count = 0
+
+        #the same row
+        for col in self.board:
+            if(col == column):
+                pass
+            else if(board[column] == board[co;]):
+                count++
+
+        #the postivie diagonal
+        for col in self.board:
+            if (col < column):
+                if (board[col] == board[column] - (column - col)):
+                    count++
+            else if (col > column):
+                if (board[col] == board[column] + (column - col)):
+                    count++
+
+        #the negative diagonal
+        for col in self.board:
+            if (col < column):
+                if (board[col] == board[column] + (column - col)):
+                    count++
+            else if (col > column):
+                if (board[col] == board[column] - (column - col)):
+                    count++
         
+        return count
+
+
+
+
 
     #print n*n board
     def print_board(self, board = None):
         for row in self.board:
             for col in self.board:
                 if(row == board[col]):
-                    print("Q", end = "\t")
+                    print("Q", end = '\t')
                 else:
-                    print('_', end="\t")
+                    print('.', end ='\t')
             print("\n")
 
 
